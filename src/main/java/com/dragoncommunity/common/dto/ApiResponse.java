56f7c5ba73f1b239ba06d2f75d7a1c439a413ac4
MegaZizon <wnsdh8846@naver.com>
2026-06-07 03:18:52 +0900

    [feat] 공통 API 응답 추가

---
+++ src/main/java/com/dragoncommunity/common/dto/ApiResponse.java
@@ -0,0 +1,20 @@
+package com.dragoncommunity.common.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ApiResponse<T> {
+
+    private final String message;
+    private final T data;
+
+    public static <T> ApiResponse<T> of(String message, T data) {
+        return new ApiResponse<>(message, data);
+    }
+
+    public static <T> ApiResponse<T> of(String message) {
+        return new ApiResponse<>(message, null);
+    }
+}
