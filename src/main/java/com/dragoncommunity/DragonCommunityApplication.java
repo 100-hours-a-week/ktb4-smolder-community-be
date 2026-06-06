cf98c80ddbb8fe554149b766a200525e2509d977
MegaZizon <wnsdh8846@naver.com>
2026-06-06 16:35:59 +0900

    [feat] 프로젝트 생성
    의존성 라이브러리 추가
    - Spring Web
    - Spring Data Jpa
    - Mysql Driver
    - Lombok
    - Validation
    스프링 부트 버전 : 3.5.14
    JAVA 버전 : 17

---
+++ src/main/java/com/dragoncommunity/DragonCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.dragoncommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DragonCommunityApplication.class, args);
+    }
+
+}
