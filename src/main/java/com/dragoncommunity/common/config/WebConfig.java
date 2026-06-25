4cd0e2cf6e246775ff0b9c3b0c213af66fe90aa9
Megazizon <wnsdh8846@naver.com>
2026-06-25 16:34:24 +0900

    [feat] CORS 설정 추가
    - application.yaml에 cors 관련 환경 변수 설정 추가
    - WebMvcConfigurer에 cors 설정 Override
    - 환경 변수 properties로 관리

--- src/main/java/com/dragoncommunity/common/config/WebConfig.java
+++ src/main/java/com/dragoncommunity/common/config/WebConfig.java
@@ -1,13 +1,19 @@
 package com.dragoncommunity.common.config;
 
+import com.dragoncommunity.common.config.properties.CorsProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import static com.dragoncommunity.common.config.constant.FileConfigConstant.*;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final CorsProperties corsProperties;
 
     /**
      * 정적 리소스 접근 설정
@@ -22,4 +28,13 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler(RESOURCE_ALLOW_URL)
                 .addResourceLocations(uploadPath);
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping(corsProperties.getAllowedPattern())
+                .allowedOrigins(corsProperties.getAllowedOrigins())
+                .allowedMethods(corsProperties.getAllowedMethods())
+                .allowCredentials(corsProperties.isAllowCredentials());
+    }
+
 }
